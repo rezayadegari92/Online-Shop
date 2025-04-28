f2e75e95087811f40122def604a5d2556fba403e
reza <yadegarireza50@gmail.com>
2025-04-28 22:37:26 +0330

    ADD:pages app added for render templates

---
+++ pages/views.py
@@ -0,0 +1,38 @@
+from django.shortcuts import render
+
+
+def home(request):
+    """Simple view to render the e-commerce homepage"""
+    return render(request, 'home.html')
+
+
+def products(request):
+    return render(request, "products.html")
+
+def product_detail(request):
+    return render(request, "product_detail.html")
+
+
+def categories(request):
+    return render(request, "categories.html")
+
+
+def category_products(request):
+    return render(request, "category_products.html")
+
+
+def brands(request):
+    return render(request, "brands.html")
+
+
+
+def brand_products(request):
+    return render(request, "brand_products.html")
+
+
+def cart(request):
+    return render(request, "cart.html")
+
+
+def top_rated(request):
+    return render(request, "top_rated.html")
