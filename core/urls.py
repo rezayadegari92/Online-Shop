6f9dd89405ed7b295beff0de2a59bb6a5bd8e749
reza <yadegarireza50@gmail.com>
2025-04-30 19:11:57 +0330

    UPDATE:core urls  updated

--- core/urls.py
+++ core/urls.py
@@ -19,16 +19,20 @@ from django.urls import path, include
 from django.conf.urls.static import static
 
 from django.conf import settings
+from rest_framework_simplejwt.views import TokenRefreshView, TokenObtainPairView
+
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
+    path('api/token/',TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path("api/token/refresh/", TokenRefreshView.as_view(), name="token-refresh"),
     path('accounts/api/', include('accounts.api.urls'), name='accounts api' ),
     path('api/', include('products.api.urls'), name='products api' ),
     path('api/addresses/', include('addresses.api.urls'), name='addresses api'),
     path("", include('accounts.urls')),
-    path("cart/api/", include('carts.api.urls'), name='cart api'),
+    path("api/cart/", include('carts.api.urls'), name='cart api'),
     path("",include("pages.urls")),
     
 ]
