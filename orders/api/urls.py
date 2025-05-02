5f5f00b4aede98051d706592737c00978a085b94
reza <yadegarireza50@gmail.com>
2025-05-02 19:29:21 +0330

    UPDATE:orders api's updated

--- orders/api/urls.py
+++ orders/api/urls.py
@@ -4,6 +4,6 @@ from django.urls import path
 from .views import OrderListView, OrderDetailView
 
 urlpatterns = [
-    path('', OrderListView.as_view(), name='order-list'),
+    path('orders/', OrderListView.as_view(), name='order-list'),
     path('<int:pk>/', OrderDetailView.as_view(), name='order-detail'),
 ]
