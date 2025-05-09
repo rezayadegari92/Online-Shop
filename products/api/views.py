92752d657038e9582f972d20484729cb8d5ed70b
reza <yadegarireza50@gmail.com>
2025-05-09 16:16:12 +0330

    UPDATE: views for api's products updated

--- products/api/views.py
+++ products/api/views.py
@@ -15,66 +15,6 @@ from .serializers import (
 
 from django.db.models import Avg
 
-# class ProductViewSet(viewsets.ReadOnlyModelViewSet):
-#     permission_classes = [permissions.AllowAny]
-#     queryset = Product.objects.annotate(
-#         avg_rating=Avg('ratings__value')
-#     )
-#     #queryset = Product.objects.all()
-#     serializer_class = ProductSerializer
-#     filter_backends = [DjangoFilterBackend, filters.OrderingFilter, filters.SearchFilter]
-#     filterset_fields = ['category', 'brand']
-#     search_fields = ['name', 'category__name', 'brand__name']
-#     ordering_fields = ['price', 'avg_rating']
-#     ordering = ['-avg_rating']
-    
-#     @action(detail=True, methods=['get'])
-#     def popular(self, request, pk=None):
-        
-#         queryset = self.get_queryset().order_by('-avg_rating')
-
-#         page = self.paginate_queryset(queryset)
-#         if page is not None:
-#             serializer = self.get_serializer(page, many=True)
-#             return self.get_paginated_response(serializer.data)
-#         serializer = self.get_serializer(queryset, many=True)
-#         return Response(serializer.data)
-    
-
-# class CommentViewSet(viewsets.ModelViewSet):
-#     queryset = Comment.objects.all().order_by('-created_at')
-#     serializer_class = CommentSerializer
-#     permission_classes = [permissions.AllowAny]
-
-#     def perform_create(self, serializer):
-#         serializer.save(author=self.request.user)
-
-#     def create(self, request, *args, **kwargs):
-#         if not request.user.is_authenticated:
-#             return Response({"error": "user must be login for put comments"})
-#         return super().create(request, *args, **kwargs)
-
-# class CategoryViewSet(viewsets.ReadOnlyModelViewSet):
-#     permission_classes = [permissions.AllowAny]
-#     queryset = Category.objects.all()
-#     serializer_class = CategorySerializer
-
-
-# class RatingViewSet(viewsets.ModelViewSet):
-#     queryset = Rating.objects.all()
-#     serializer_class = ProductRatingSerializer
-#     permission_classes = [permissions.AllowAny]
-
-#     def perform_create(self, serializer):
-#         product = serializer.validated_data['product']
-#         user = self.request.user
-
-#         rating , created = Rating.objects.get_or_create(
-#             product=product,
-#             user=user,
-#             defaults={'value': serializer.validated_data['value']}
-#         )
-#         serializer.instance = rating
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
