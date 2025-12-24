72f4cb45659fbbf0c681fa65bc3adbf871fefce1
rezayadegari92 <yadegarireza50@gmail.com>
2025-12-24 12:28:30 +0330

    UPDATE: add REDIS_URL environment variable to docker-compose and adjust Redis cache location in settings.py for improved configuration management

--- Online-Shop/core/settings.py
+++ Online-Shop/core/settings.py
@@ -200,7 +200,7 @@ CELERY_BROKER_CONNECTION_MAX_RETRIES = 10
 CACHES = {
     "default": {
         "BACKEND": "django_redis.cache.RedisCache",
-        "LOCATION": os.getenv("REDIS_URL", "redis://redis:6379/1"),
+        "LOCATION": os.getenv("REDIS_URL"),
         "OPTIONS": {
             "CLIENT_CLASS": "django_redis.client.DefaultClient",
             "CONNECTION_POOL_KWARGS": {
